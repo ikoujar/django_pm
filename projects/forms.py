974cc30519c248f9993bcac119b9911a44fd920c
Ibrahim Koujar <ibrahimkoujar92@gmail.com>
2021-12-09 01:52:30 +0300

    Add RTL & Arabic support

--- projects/forms.py
+++ projects/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from . import models
+from django.utils.translation import gettext as _
+
 
 attrs = {'class': 'form-control'}
 
@@ -8,6 +10,11 @@ class ProjectCreateForm(forms.ModelForm):
     class Meta:
         model = models.Project
         fields = ['category', 'title', 'description']
+        labels = {
+            'category': _('Category'),
+            'title': _('Title'),
+            'description': _('Description'),
+        }
         widgets = {
             'category': forms.Select(attrs=attrs),
             'title': forms.TextInput(attrs=attrs),
